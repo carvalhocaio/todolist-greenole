20de904e5052160aa1339b82e0d64097e1565535
Caio Carvalho <caiocarvalho.py@gmail.com>
2025-05-14 13:00:37 -0400

    refactor tests in test_response.py to improve setup and add more assertions

--- todo/tests/test_response.py
+++ todo/tests/test_response.py
@@ -1,11 +1,15 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
+from todo import models
+from django.core.cache import cache
 
 
 class ToDoTestCase(APITestCase):
     def setUp(self):
+        cache.clear()
+
         self.list_url = reverse("todo-list")
-        data = [
+        self.todos_data = [
             {"title": "ToDo Test 001", "description": "testing response A"},
             {
                 "title": "ToDo Test 002",
@@ -18,45 +22,59 @@ class ToDoTestCase(APITestCase):
                 "status": "CONCLUDED",
             },
         ]
-        self.client.post(self.list_url, data=data)
+        
+        for todo in self.todos_data:
+            models.ToDo.objects.create(**todo)
 
-    def test_response_get_to_list_todos(self):
+    def test_list_todos(self):
         response = self.client.get(self.list_url)
-        expected_response = {
-            "count": 3,
-            "next": None,
-            "previous": None,
-            "results": [
-                {
-                    "id": response.data["results"][0]["id"],
-                    "status_display": "concluded",
-                    "title": "ToDo Test 003 searching",
-                    "description": "testing response C",
-                },
-                {
-                    "id": response.data["results"][1]["id"],
-                    "status_display": "concluded",
-                    "title": "ToDo Test 002",
-                    "description": "testing response B searching",
-                },
-                {
-                    "id": response.data["results"][2]["id"],
-                    "status_display": "in progress",
-                    "title": "ToDo Test 001",
-                    "description": "testing response A",
-                },
-            ],
-        }
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["count"], len(self.todos_data))
 
-        actual_results = response.data["results"]
-        for result in actual_results:
-            result.pop("created_at", None)
-            result.pop("updated_at", None)
+    def test_filter_todos_by_status(self):
+        response = self.client.get(f"{self.list_url}?status=CONCLUDED")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["count"], 2)
+        for todo in response.data["results"]:
+            self.assertEqual(todo["status_display"], "concluded")
 
-        expected_results = expected_response["results"]
+    def test_filter_todos_by_search(self):
+        response = self.client.get(f"{self.list_url}?search=searching")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["count"], 2)
+        for todo in response.data["results"]:
+            self.assertIn("searching", todo["title"] + todo["description"])
 
-        self.assertEqual(actual_results, expected_results)
-        self.assertEqual(response.data, expected_response)
+    def test_retrieve_todo_by_id(self):
+        todo = self.client.post(self.list_url, {"title": "ToDo Test 004"}).data
+        response = self.client.get(f"{self.list_url}/{todo['id']}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["id"], todo["id"])
+
+    def test_create_todo(self):
+        data = {"title": "ToDo Test 005", "description": "New ToDo"}
+        response = self.client.post(self.list_url, data=data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data["title"], data["title"])
+
+    def test_create_todo_with_invalid_status(self):
+        data = {"title": "ToDo Test", "status": "INVALID"}
+        response = self.client.post(self.list_url, data=data)
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("error", response.data)
+
+    def test_update_todo_status(self):
+        todo = self.client.post(self.list_url, {"title": "ToDo Test 006"}).data
+        response = self.client.patch(f"{self.list_url}/{todo['id']}", {"status": "CONCLUDED"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["status_display"], "concluded")
+
+    def test_delete_todo(self):
+        todo = self.client.post(self.list_url, {"title": "ToDo Test 007"}).data
+        response = self.client.delete(f"{self.list_url}/{todo['id']}")
+        self.assertEqual(response.status_code, 204)
+        get_response = self.client.get(f"{self.list_url}/{todo['id']}")
+        self.assertEqual(get_response.status_code, 404)
 
     def test_response_get_to_list_with_status_concluded(self):
         response = self.client.get(f"{self.list_url}?status=CONCLUDED")
@@ -254,3 +272,11 @@ class ToDoTestCase(APITestCase):
 
         response = self.client.delete(f"{self.list_url}/{todo_id}")
         self.assertEqual(response.data, None)
+
+    def test_setup_creates_todos(self):
+        self.assertEqual(models.ToDo.objects.count(), len(self.todos_data))
+
+    def test_list_api_returns_all_todos(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["count"], models.ToDo.objects.count())
